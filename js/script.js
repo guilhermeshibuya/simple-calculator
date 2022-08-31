6f0f827e18460dbb0608e1dd4304af4edceab09a
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2022-08-30 23:29:41 -0300

    Calculator class created, some methods partially implemented

--- js/script.js
+++ js/script.js
@@ -2,26 +2,55 @@ class Calculator {
     constructor(previousOperandTxt, currentOperandTxt) {
         this.previousOperandTxt = previousOperandTxt;
         this.currentOperandTxt = currentOperandTxt;
+        this.clear();
     }  
 
     clear() {
         this.previousOperand = '';
         this.currentOperand = '';
+       
     }
 
     delete() {
-        
+
+    }
+
+    appendNumber(number) {
+        this.currentOperand = this.currentOperandTxt.innerText + number;
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        // this.previousOperandTxt.innerText = this.previousOperand;
+        this.currentOperandTxt.innerText = this.currentOperand;
     }
 }
 
 const numberBtns = document.querySelectorAll('[data-number]');
-const operation = document.querySelectorAll('[data-operation');
-const equals = document.querySelector('[data-equals');
+const operation = document.querySelectorAll('[data-operation]');
+const equals = document.querySelector('[data-equals]');
 const del = document.querySelector('[data-del]');
 const ac = document.querySelector('[data-ac]');
-const currentOperandTxt = document.querySelector('[data-currrent-operand]');
+const currentOperandTxt = document.querySelector('[data-current-operand]');
 const previousOperandTxt = document.querySelector('[data-previous-operand]');
 
 const calc = new Calculator(previousOperandTxt, currentOperandTxt);
 
-ac.addEventListener('click', calc.clear());
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calc.appendNumber(button.innerText);
+        calc.updateDisplay();
+    })
+})
+
+ac.addEventListener('click', () => {
+    calc.clear();
+    calc.updateDisplay();
+})
